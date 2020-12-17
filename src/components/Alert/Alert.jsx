3b65fb1d9a65947b1cb3a266cde2e4b52f7a35a4
Омельянов Дима <domelyanov@hotmail.com>
2020-12-17 02:04:13 +0200

    c

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const Alert = (props) => { 
-
+const Alert = (props) => {
   return (
-    <div><h2>This contact is already exist!</h2><button onClick={props.ok}>OK</button></div>
-  )
-  
+    <div>
+      <h2 className="title">This contact is already exist!</h2>
+      <button onClick={props.ok} className="btn">
+        OK
+      </button>
+    </div>
+  );
 };
 
 export default Alert;
--- src/components/Contascts/Contacts.jsx
+++ src/components/Contascts/Contacts.jsx
@@ -1,41 +1,40 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import {TransitionGroup, CSSTransition} from 'react-transition-group';
-import './Contacts.css';
-import {deleteContacts} from '../../redux/operations/operations';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import "./Contacts.css";
+import { deleteContacts } from "../../redux/operations/operations";
+import { connect } from "react-redux";
 
 class Contacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.arrayOf(PropTypes.object),
+    deleteContacts: PropTypes.func,
+  };
 
-    static propTypes = {
-        contacts: PropTypes.arrayOf(PropTypes.object),
-        deleteContacts: PropTypes.func,
-      };
+  render() {
+    const { contacts, deleteContacts } = this.props;
 
-    render() {
-        
-        const {contacts, deleteContacts} = this.props;
-        
-        return (
-            <TransitionGroup component="ul">
-
-                {
-                    contacts.map(el => {
-                        return (
-                            <CSSTransition key={el.id} timeout={250} classNames="contact">
-                                <li >{el.name}: {el.number} <button type="button" onClick={() => deleteContacts(el.id)}>Delete</button></li>
-                            </CSSTransition>
-                        )
-                    })
-                }
-
-            </TransitionGroup>
-    )}
-
-};
+    return (
+      <TransitionGroup component="ul" className="list">
+        {contacts.map((el) => {
+          return (
+            <CSSTransition key={el.id} timeout={250} classNames="contact">
+              <li className="item">
+                {el.name}: {el.number}{" "}
+                <button type="button" onClick={() => deleteContacts(el.id)}>
+                  Delete
+                </button>
+              </li>
+            </CSSTransition>
+          );
+        })}
+      </TransitionGroup>
+    );
+  }
+}
 
 const mapDispatchToProps = {
-    deleteContacts,
+  deleteContacts,
 };
 
 export default connect(null, mapDispatchToProps)(Contacts);
