d4e5b762f11b13d0f200fba5ab99d92ffa975adc
Омельянов Дима <domelyanov@hotmail.com>
2020-12-18 14:17:42 +0200

    c

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,80 +1,85 @@
-import React, {Component} from 'react';
-import './App.css';
-import Form from '../Form/Form';
-import Contacts from '../Contascts/Contacts';
-import Filter from '../Filter/Filter';
-import Alert from '../Alert/Alert';
-import {CSSTransition} from 'react-transition-group';
-import {connect} from 'react-redux';
-import {getContacts} from '../../redux/operations/operations';
-import {getContactsArr, getFilter} from '../../redux/selectors';
+import React, { Component } from "react";
+import "./App.css";
+import Form from "../Form/Form";
+import Contacts from "../Contascts/Contacts";
+import Filter from "../Filter/Filter";
+import Alert from "../Alert/Alert";
+import { CSSTransition } from "react-transition-group";
+import { connect } from "react-redux";
+import { getContacts } from "../../redux/operations/operations";
+import { getContactsArr, getFilter } from "../../redux/selectors";
 
 class App extends Component {
+  state = {
+    showLogo: false,
+    contactExist: false,
+  };
 
-    state = {
-        showLogo: false,
-        contactExist: false
-    };
+  componentDidMount() {
+    this.props.getContacts();
+    this.setState((state) => ({ showLogo: !state.showLogo }));
+  }
 
-    componentDidMount() {
-        this.props.getContacts();
-        this.setState(state => ({showLogo: !state.showLogo}));
-    };
+  findContact = () => {
+    const { contacts, filterWord } = this.props;
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filterWord.toLowerCase())
+    );
+  };
 
-    findContact = () => {
-        const {contacts, filterWord} = this.props;
-            return contacts.filter(contact => contact.name.toLowerCase().includes(filterWord.toLowerCase()),
-        );
-    };
+  checkContact = (contacts, contact) => {
+    if (contacts.find((el) => el.name === contact.name)) {
+      this.setState({ contactExist: true });
+      return true;
+    }
 
-    checkContact = (contacts, contact) => {
+    return false;
+  };
 
-        if (contacts.find((el) => el.name === contact.name)) {
-            this.setState({contactExist: true}) 
-            return true;
-        };
+  alertOk = () => {
+    this.setState({ contactExist: false });
+  };
 
-        return false;
-        
-    };
+  render() {
+    const { showLogo, contactExist } = this.state;
+    const { filter } = this.props;
+    const searchedContacts = this.findContact();
 
-    alertOk = () => {
-        this.setState({contactExist: false})
-    };
-
-    render() {
-
-        const {showLogo, contactExist} = this.state;
-        const {filter} = this.props;
-        const searchedContacts = this.findContact();
-
-        return (
-            <>
-                <CSSTransition in={showLogo} unmountOnExit classNames="logo" timeout={500}>
-                    <h1>Phonebook</h1>
-                </CSSTransition>
-                <h2>Contacts form</h2>
-                <Form checkContact={this.checkContact} />
-                <h2>Contacts list</h2>
-                <Filter filter={filter} handleFilter={this.handleFilter} />
-                <Contacts contacts={searchedContacts} />
-                <CSSTransition in={contactExist} unmountOnExit classNames="alert" timeout={250}>
-                    <Alert ok={this.alertOk} />
-                </CSSTransition>
-            </>
-        );
-
-    };
-
-};
+    return (
+      <>
+        <CSSTransition
+          in={showLogo}
+          unmountOnExit
+          classNames="logo"
+          timeout={500}
+        >
+          <h1>Phonebook</h1>
+        </CSSTransition>
+        <h2>Contacts form</h2>
+        <Form checkContact={this.checkContact} />
+        <h2>Contacts list</h2>
+        <Filter filter={filter} handleFilter={this.handleFilter} />
+        <Contacts contacts={searchedContacts} />
+        <CSSTransition
+          in={contactExist}
+          unmountOnExit
+          classNames="alert"
+          timeout={250}
+        >
+          <Alert ok={this.alertOk} />
+        </CSSTransition>
+      </>
+    );
+  }
+}
 
 const mapDispatchToProps = {
-    getContacts
+  getContacts,
 };
 
-const mapStateToProps = state => ({
-    contacts: getContactsArr(state),
-    filterWord: getFilter(state),
+const mapStateToProps = (state) => ({
+  contacts: getContactsArr(state),
+  filterWord: getFilter(state),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
